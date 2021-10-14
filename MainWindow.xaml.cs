6aa7d9b19dfebdbbf56e1c169d11164e0d924fae
Sergey Nadeev <nadeevsa@mail.ru>
2021-10-14 20:16:24 +0300

    Добавить стакан

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,44 @@ using System.Windows.Shapes;
 
 namespace HMID
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public class Ctakan
+        {
+            // TODO: Написать название и сам класс нормально
+            public double number1 { get; set; }
+            public double number2 { get; set; }
+            public Brush color { get; set; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            // TODO: Сделать сбор данных из БД
+            List<Ctakan> items = new List<Ctakan>();
+            Random random = new Random();
+            for (int i = 1; i <= 14; ++i)
+            {
+                lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100)/random.Next(1, 3), number2 = (double)random.Next(100)/random.Next(1, 3), color = Brushes.Pink });
+            }
+            lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.Red });
+            for (int i = 1; i <= 5; ++i)
+            {
+                lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100)/random.Next(1, 3), number2 = (double)random.Next(100)/random.Next(1, 3), color = Brushes.White });
+            }
+            lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.Green });
+            for (int i = 1; i <= 19; ++i)
+            {
+                lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.LightGreen });
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кнопка нажата");
+            ListBox listBox = new ListBox();
+            listBox.Name = "lbTodoList";
+            listBox.Width = 150;
         }
     }
 }
