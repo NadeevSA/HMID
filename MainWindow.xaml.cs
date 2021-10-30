a7b4a9cc790daa71cb9cc3b667109bf1f51afb12
Sergey Nadeev <nadeevsa@mail.ru>
2021-10-30 21:02:13 +0300

    Создавать стаканы по кнопке

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,30 +29,37 @@ namespace HMID
         {
             InitializeComponent();
             // TODO: Сделать сбор данных из БД
-            List<Ctakan> items = new List<Ctakan>();
-            Random random = new Random();
-            for (int i = 1; i <= 14; ++i)
-            {
-                lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100)/random.Next(1, 3), number2 = (double)random.Next(100)/random.Next(1, 3), color = Brushes.Pink });
-            }
-            lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.Red });
-            for (int i = 1; i <= 5; ++i)
-            {
-                lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100)/random.Next(1, 3), number2 = (double)random.Next(100)/random.Next(1, 3), color = Brushes.White });
-            }
-            lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.Green });
-            for (int i = 1; i <= 19; ++i)
-            {
-                lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.LightGreen });
-            }
+            //Random random = new Random();
+            //for (int i = 1; i <= 14; ++i)
+            //{
+            //    lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100)/random.Next(1, 3), number2 = (double)random.Next(100)/random.Next(1, 3), color = Brushes.Pink });
+            //}
+            //lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.Red });
+            //for (int i = 1; i <= 5; ++i)
+            //{
+            //    lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100)/random.Next(1, 3), number2 = (double)random.Next(100)/random.Next(1, 3), color = Brushes.White });
+            //}
+            //lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.Green });
+            //for (int i = 1; i <= 19; ++i)
+            //{
+            //    lbTodoList.Items.Add(new Ctakan() { number1 = (double)random.Next(100) / random.Next(1, 3), number2 = (double)random.Next(100) / random.Next(1, 3), color = Brushes.LightGreen });
+            //}
         }
 
+        bool f = true;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Кнопка нажата");
+            //lbTodoList1.Items.Add(new Ctakan() { number1 = 10, number2 = 5, color = Brushes.LightGreen });
             ListBox listBox = new ListBox();
-            listBox.Name = "lbTodoList";
-            listBox.Width = 150;
+            if (f == true)
+            {
+                WrapPanel dynamicWrapPanel = new WrapPanel();
+                f = false;
+                listBox.Name = "asd";
+                listBox.Width = 140;
+                listBox.Items.Add(new Ctakan() { number1 = 11, number2 = 5, color = Brushes.LightGreen });
+                panel1.Children.Add(listBox);
+            }
         }
     }
 }
